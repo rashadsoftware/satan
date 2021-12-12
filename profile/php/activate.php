bcffc80b41409dad7059c85d7c588473650a62b7
Rashad Alakbarov <rashadalakbarov9@gmail.com>
2021-12-12 17:09:22 +0400

    update activate system in profile

--- profile/php/activate.php
+++ profile/php/activate.php
@@ -6,10 +6,23 @@
 		$data=array();
 
 		$inputHiddenID=$_POST["getID"];
+
+        date_default_timezone_set("Asia/Baku");
+        $nowTime=time();
+        $timeWeb=date('Y-m-d H:i:s', $nowTime);
+        $deadline=date('Y-m-d H:i:s', strtotime("+1 month", $nowTime));
 		
-		$updateElan=mysqli_query($connect,"UPDATE elan SET elan_status='active' WHERE elan_id = '$inputHiddenID'");
+		$updateElan=mysqli_query($connect,"UPDATE elan SET elan_status='active', elan_time='$timeWeb' WHERE elan_id = '$inputHiddenID'");
 
         if($updateElan){
+            // remove deadline
+            $allDeadlineElan=mysqli_query($connect, "SELECT * FROM deadline WHERE elan_id='$inputHiddenID'");
+            if(mysqli_num_rows($allDeadlineElan) > 0){
+                mysqli_query($connect, "DELETE FROM deadline WHERE elan_id='$inputHiddenID'");
+            }
+
+            mysqli_query($connect,"INSERT IGNORE INTO deadline (elan_id, deadline_time) VALUES ('$inputHiddenID', '$deadline')");
+
             $data["ok"]="ok";  
             $data["text"]="Təbriklər! Elanı aktivləşdirdiniz";              
             echo json_encode($data);
